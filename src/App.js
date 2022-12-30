9b472a38d11155720ba4a204bee20fcd06d23f0d
Klajdi <klajdixhafkollari36@gmail.com>
2022-12-30 17:12:32 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// using class component
 import React, {Component} from 'react';
 import SearchBar from './Component/SearchBar/SearchBar';
 import Cardlist from './Component/Card/Cardlist';
@@ -74,3 +75,68 @@ class App extends Component {
 }
 
 export default App;
+
+// using functional component
+// import React, { useState } from "react";
+// import SearchBar from "./Component/SearchBar/SearchBar";
+// import Cardlist from "./Component/Card/Cardlist";
+// import Error from "./Component/Assets/Error";
+// import Loading from "./Component/Assets/Loading";
+
+// const App = () => {
+//   const [searchResult, setSearchResult] = useState("");
+//   const [searchButtonPress, setSearchButtonPress] = useState(false);
+//   const [totalHits, setTotalHits] = useState(null);
+//   const [isLoading, setIsLoading] = useState(false);
+  
+//   const onSearchChangeHandle = (event) => {
+//     if (event.target.value.trim().length > 2) {
+//       event.preventDefault();
+//       setSearchButtonPress(true);
+//       setIsLoading(true);
+//       fetch(`https://en.wikipedia.org//w/api.php?origin=*&action=query&format=json&list=search&utf8=1&srsearch=${event.target.value.trim()}`)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setSearchResult(data.query.search);
+//         setTotalHits( data.query.searchinfo.totalHits);
+//         setIsLoading(false);
+//       })
+//       .catch()
+//     } else {
+//       setSearchResult([]);
+//     }
+//   };
+
+//   if (totalHits === 0) {
+//     return (
+//       <div>
+//         <SearchBar searchChange={onSearchChangeHandle}/>
+//         <Error/>
+//       </div>
+//     );
+//   };
+//   if(searchButtonPress) {
+//     if(isLoading) {
+//       return (
+//         <div>
+//           <SearchBar searchChange={onSearchChangeHandle}/>
+//           <Loading/>
+//         </div>
+//       );
+//     } else {
+//       return (
+//         <div>
+//           <SearchBar searchChange={onSearchChangeHandle}/>
+//           <Cardlist SearchResult={searchResult}/>
+//         </div>
+//       );
+//     }
+//   };
+//   return (
+//     <div> 
+//       <SearchBar searchChange={onSearchChangeHandle}/>
+//     </div>
+//   );
+// };
+
+// export default App;
--- src/Component/Assets/Error.js
+++ src/Component/Assets/Error.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Loading.css';
+import './Error.css';
 
 const Error = () => {
     return(
-        <header className="tc ph5 lh-copy">
+        <header className="tc ph5 lh-copy" id='error-header'>
             <h2 className="tc f1-l fw1 white">Sorry, we can't find the results you are looking for</h2>
         </header>
     );
