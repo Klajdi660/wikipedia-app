67b00df4016f8a6517f36a31438769f2860d0534
Klajdi <klajdixhafkollari36@gmail.com>
2023-10-09 16:58:11 +0200

    chngs3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import SearchBar from "./pages/SearchBar";
 import Cardlist from "./component/Card/Cardlist";
 import Error from "./component/common/Error";
@@ -10,23 +11,48 @@ const App = () => {
   const [totalHits, setTotalHits] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  const onSearchChange = (event) => {
-    if (event.target.value.trim().length > 2) {
-      event.preventDefault();
-      setSearchButtonPress(true);
-      setIsLoading(true);
-      fetch(
-        `https://en.wikipedia.org//w/api.php?origin=*&action=query&format=json&list=search&utf8=1&srsearch=${event.target.value.trim()}`
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          setSearchResult(data.query.search);
-          setTotalHits(data.query.searchinfo.totalhits);
-          setIsLoading(false);
-        })
-        .catch();
-    } else {
+  // const onSearchChange = async (event) => {
+  //   if (event.target.value.trim().length > 2) {
+  //     event.preventDefault();
+  //     setSearchButtonPress(true);
+  //     setIsLoading(true);
+  //     fetch(
+  //       `https://en.wikipedia.org//w/api.php?origin=*&action=query&format=json&list=search&utf8=1&srsearch=${event.target.value.trim()}`
+  //     )
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         setSearchResult(data.query.search);
+  //         setTotalHits(data.query.searchinfo.totalhits);
+  //         setIsLoading(false);
+  //       })
+  //       .catch();
+  //   } else {
+  //     setSearchResult([]);
+  //   }
+  // };
+  
+  const onSearchChange = async (event) => {
+    const searchTerm = event.target.value.trim();
+
+    if (searchTerm.length <= 2) {
       setSearchResult([]);
+      return;
+    }
+
+    setSearchButtonPress(true);
+    setIsLoading(true);
+
+    try {
+      const response = await axios.get(
+        `https://en.wikipedia.org//w/api.php?origin=*&action=query&format=json&list=search&utf8=1&srsearch=${searchTerm}`
+      );
+
+      const { data } = response;
+      setSearchResult(data.query.search);
+      setTotalHits(data.query.searchinfo.totalhits);
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Error:', error);
     }
   };
 
